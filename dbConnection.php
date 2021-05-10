d4e92eba3b228cb08eff6e0cfd105d864950cc1c
rgrajat085 <rajatg62@gmail.com>
2021-05-10 20:35:55 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,5 +1,5 @@
 <?php
 //all the variables defined here are accessible in all the files that include this one
-$con= new mysqli('localhost','root','','project')or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('localhost','epiz_28584263','fs6s6k0b1SkN','epiz_28584263_navigus')or die("Could not connect to mysql".mysqli_error($con));
 
 ?>
